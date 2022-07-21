5c534df4a977e7e58a61f35dc0d9a0f179c927ed
Martin Morissette <martin.morissette@shopify.com>
2022-07-20 21:10:10 -0400

    Base project

---
+++ lib/notion/to/logseq.rb
@@ -0,0 +1,10 @@
+require "notion/to/logseq/version"
+
+module Notion
+  module To
+    module Logseq
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/notion/to/logseq/version.rb
@@ -0,0 +1,7 @@
+module Notion
+  module To
+    module Logseq
+      VERSION = "0.1.0"
+    end
+  end
+end
